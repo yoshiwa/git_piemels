701e7278e54f1db470c543ca05f35ae0bcff3f90
yoshiwa <stephan_creelle@hotmail.com>
2020-02-13 20:44:18 +0100

    mainahead

--- mqtt_esp8266/mqtt_esp8266.ino
+++ mqtt_esp8266/mqtt_esp8266.ino
@@ -28,7 +28,7 @@
 
 // Update these with values suitable for your network.
 
-const char* ssid = "........";
+const char* ssid = "zaaag";
 const char* password = "........";
 const char* mqtt_server = "broker.mqtt-dashboard.com";
 
